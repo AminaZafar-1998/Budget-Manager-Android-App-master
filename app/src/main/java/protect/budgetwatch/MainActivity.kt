728b16476ae9d4fafed4513d7c45a49a6304baac
AminaZafar-1998 <68804288+AminaZafar-1998@users.noreply.github.com>
2020-07-28 08:31:45 +0500

    Update MainActivity.kt

--- app/src/main/java/protect/budgetwatch/MainActivity.kt
+++ app/src/main/java/protect/budgetwatch/MainActivity.kt
@@ -2,36 +2,42 @@ package protect.budgetwatch
 
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import android.util.Log
 import android.view.*
-import android.webkit.WebView
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.ListView
 import android.widget.TextView
-import com.google.common.collect.ImmutableMap
-import protect.budgetwatch.BudgetActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import protect.budgetwatch.ImportExportActivity
 import protect.budgetwatch.intro.IntroActivity
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
         val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
         val menuItems: MutableList<MainMenuItem> = LinkedList()
-        menuItems.add(MainMenuItem(R.drawable.purse, R.string.budgetsTitle,
-                R.string.budgetDescription))
-        menuItems.add(MainMenuItem(R.drawable.transaction, R.string.transactionsTitle,
-                R.string.transactionsDescription))
+        menuItems.add(
+            MainMenuItem(
+                R.drawable.purse, R.string.budgetsTitle,
+                R.string.budgetDescription
+            )
+        )
+        menuItems.add(
+            MainMenuItem(
+                R.drawable.transaction, R.string.transactionsTitle,
+                R.string.transactionsDescription
+            )
+        )
         val buttonList = findViewById<View>(R.id.list) as ListView
         val buttonListAdapter = MenuAdapter(this, menuItems)
         buttonList.adapter = buttonListAdapter
@@ -50,6 +56,7 @@ class MainActivity : AppCompatActivity() {
             if (goalClass != null) {
                 val i = Intent(applicationContext, goalClass)
                 startActivity(i)
+
             }
         }
         val prefs = getSharedPreferences("protect.budgetwatch", Context.MODE_PRIVATE)
@@ -61,9 +68,10 @@ class MainActivity : AppCompatActivity() {
 
     internal class MainMenuItem(val iconId: Int, val menuTextId: Int, val menuDescId: Int)
 
-    internal class MenuAdapter(context: Context?, items: List<MainMenuItem>?) : ArrayAdapter<MainMenuItem?>(
-        context!!, 0, items!!
-    ) {
+    internal class MenuAdapter(context: Context?, items: List<MainMenuItem>?) :
+        ArrayAdapter<MainMenuItem?>(
+            context!!, 0, items!!
+        ) {
         override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
             // Get the data item for this position
             var convertView = convertView
@@ -71,15 +79,18 @@ class MainActivity : AppCompatActivity() {
 
             // Check if an existing view is being reused, otherwise inflate the view
             if (convertView == null) {
-                convertView = LayoutInflater.from(context).inflate(R.layout.main_button,
-                        parent, false)
+                convertView = LayoutInflater.from(context).inflate(
+                    R.layout.main_button,
+                    parent, false
+                )
             }
-            val menuText = convertView!!.findViewById<View>(R.id.menu) as TextView
-            val menuDescText = convertView!!.findViewById<View>(R.id.menudesc) as TextView
-            val icon = convertView!!.findViewById<View>(R.id.image) as ImageView
+            val menuText = convertView?.findViewById<View>(R.id.menu) as TextView
+            val menuDescText = convertView?.findViewById<View>(R.id.menudesc) as TextView
+            val icon = convertView?.findViewById<View>(R.id.image) as ImageView
             menuText.setText(item!!.menuTextId)
             menuDescText.setText(item.menuDescId)
             icon.setImageResource(item.iconId)
+
             return convertView
         }
     }
@@ -90,6 +101,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
         val id = item.itemId
         if (id == R.id.action_import_export) {
             val i = Intent(applicationContext, ImportExportActivity::class.java)
@@ -105,17 +117,15 @@ class MainActivity : AppCompatActivity() {
             startIntro()
             return true
         }
-       
-         
+
         return super.onOptionsItemSelected(item)
     }
-
-    
 
     private fun startIntro() {
         val intent = Intent(this, IntroActivity::class.java)
         startActivity(intent)
     }
+
 
     companion object {
         private const val TAG = "BudgetWatch"
